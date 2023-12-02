a74cae890b0cd370a8a0e5f02e0960f274295cc2
ImeGits <udomary963@gmail.com>
2023-12-02 06:46:10 -0800

    Loop

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,4 +2,4 @@
 # Write a function that computes a to the power of b and return the value.
 
 def pow(a, b):
-    return (a * b)
+    return (a ** b)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Write a function that prints a string in uppercase followed by a new line.
+# Author - Tolulope Fakunle
 
 def uppercase(str):
+    """Print a string in uppercase."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
