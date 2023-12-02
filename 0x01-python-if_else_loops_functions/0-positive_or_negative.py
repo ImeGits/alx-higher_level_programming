f18c5f8d8e6134df404f87fa5973eb10a3db6805
ImeGits <udomary963@gmail.com>
2023-12-02 04:48:46 -0800

    Loops

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#Write a program that will assign a random signed number to the variable number each time it is executed.
 import random
 number = random.randint(-10, 10)
 if number > 0:
@@ -7,4 +6,4 @@ if number > 0:
 elif number == 0:
     print(f"{number:d} is zero")
 else:
-    print(f"{number:d} is negative")
+    print(f"{number:d} is negative")    
