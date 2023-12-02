2f6fc0d28455eaeaedd3ecb0c300331d16f5c9ff
ImeGits <udomary963@gmail.com>
2023-12-02 04:58:04 -0800

    Loops

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#Write a program that will assign a random signed number to the variable number each time it is executed.
 import random
 number = random.randint(-10000, 10000)
 figure = abs(number) % 10
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Print all letters from the ASCII alphabet except q and e.
+# Print all letters from the ASCII alphabet except q and e.
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
