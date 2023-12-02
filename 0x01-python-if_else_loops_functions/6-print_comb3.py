de20f0d20bba5a3fb4dadfa9e5463ca0b2a70c36
ImeGits <udomary963@gmail.com>
2023-12-02 05:47:28 -0800

    Loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Write a program that prints all possible different combinations of two digits.
+# Write a program that prints all possible combinations of two digits.
 # Numbers must be separated by ,, followed by a space.
 # The two digits must be different.
 # 01 and 10 are considered the same combination of the two digits 0 and 1.
